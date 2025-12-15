9c521650a517f4797c6600d2f781bdfe74b79b88
OlgaPankeeva <0lga.pankeeva@mail.ru>
2025-12-15 21:55:19 +0400

    add

---
+++ src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //universalMethod();
+    }
+
+    public static void universalMethod(String str){
+
+    }
+
+    //напишите тут ваш код
+    public static void universalMethod(int a){
+
+    }
+    public static void universalMethod(int b, int c){
+
+    }
+    public static void universalMethod(int[] date, int value){
+
+    }
+    public static void universalMethod(String str1, String str2){
+
+    }
+    public static void universalMethod(int y, String str3){
+
+    }
+    public static void universalMethod(String text, int count){
+
+    }
+    public static void universalMethod(int[][][] date, int value){
+
+    }
+    public static void universalMethod(int [][][][]date, int value){
+
+    }
+            public static void universalMethod(int [][][][] date, int[][][] date1){
+
+    }
+}
